4425c8cad6d14419b2a4248d543b5124db7d6c6b
Pedro Holguin <holguinpedro90@gmail.com>
2024-10-28 00:26:53 -0500

    :fire: add projects

--- src/app/utils/projects.ts
+++ src/app/utils/projects.ts
@@ -4,7 +4,7 @@ export const projects: projectI[] = [
     description:
       'Me encargué del desarrollo de interfaces con flutter y desarrollo de Apis y servicios en Nestjs, implementación de base de datos en PostgreSQL, además de la implementación de un sistema de autenticación con JWT.',
     color: '#7269ff',
-    images: ['./assets/images/portfolio/taclia-capture.jpg'],
+    images: ['./assets/images/portfolio/app-citas-medicas.png'],
     technologies: [
       'devicon-flutter-plain',
       'devicon-nestjs-original',
@@ -12,21 +12,19 @@ export const projects: projectI[] = [
       'devicon-rxjs-plain',
       'devicon-postgresql-plain',
     ],
-    url: 'https://www.taclia.com/',
   },
   {
     title: 'Proyecto de Sistema de Inventario e incidencias',
     description:
       'Es un sistema de web para registro de incidencias y control de inventario, también el traspaso de material entre sedes, desarrollado con Angular 17 y Nestjs, implementación de base de datos en PostgreSQL, además de la implementación de un sistema de autenticación con JWT.',
     color: '#63cbb2',
-    images: ['./assets/images/portfolio/dondoctor-capture.jpg'],
+    images: ['./assets/images/portfolio/inventario.png'],
     technologies: [
       'devicon-react-original',
       'devicon-typescript-plain',
       'devicon-mysql-original',
       'devicon-nodejs-plain-wordmark',
     ],
-    url: 'https://www.dondoctor.com/',
   },
   {
     title: 'Sistema de gestión de ventas e inventario para Óptica',
@@ -42,7 +40,6 @@ export const projects: projectI[] = [
       'devicon-postgresql-plain',
       'devicon-docker-plain',
     ],
-    url: 'https://www.youtube.com/watch?v=kyZ7U-El2OY',
   },
 
   {
@@ -50,7 +47,7 @@ export const projects: projectI[] = [
     description:
       'Una aplicación web diseñada para gestionar ventas e inventario de una óptica y optometría el cual se está implementando microservicios con NestJS y Nats para la comunicación entre los servicios, además de la implementación de un sistema de autenticación con JWT.',
     color: '#ff6969',
-    images: ['https://e.rpp-noticias.io/large/2015/02/02/1466486.jpg'],
+    images: ['./assets/images/portfolio/sunat.png'],
     technologies: [
       'devicon-nestjs-original',
       'devicon-typescript-plain',
@@ -59,7 +56,6 @@ export const projects: projectI[] = [
       'devicon-postgresql-plain',
       'devicon-docker-plain',
     ],
-    url: 'https://www.youtube.com/watch?v=kyZ7U-El2OY',
   },
 
   {
@@ -67,7 +63,7 @@ export const projects: projectI[] = [
     description:
       'Una aplicación nativa para android para el control de asistencia de alumnos el cual está implementado con Android Studio en Java con APi hecha en Laravel.',
     color: '#ff6969',
-    images: ['https://e.rpp-noticias.io/large/2015/02/02/1466486.jpg'],
+    images: ['./assets/images/portfolio/app-qr.png'],
     technologies: [
       'devicon-nestjs-original',
       'devicon-typescript-plain',
@@ -76,7 +72,6 @@ export const projects: projectI[] = [
       'devicon-postgresql-plain',
       'devicon-docker-plain',
     ],
-    url: 'https://www.youtube.com/watch?v=kyZ7U-El2OY',
   },
 
   {
@@ -84,7 +79,7 @@ export const projects: projectI[] = [
     description:
       'Una aplicación web para el control de pedidos de un restaurante el cual está implementado con PHP Nativo.',
     color: '#ff6969',
-    images: ['https://e.rpp-noticias.io/large/2015/02/02/1466486.jpg'],
+    images: ['./assets/images/portfolio/restaurante.png'],
     technologies: [
       'devicon-nestjs-original',
       'devicon-typescript-plain',
@@ -93,7 +88,6 @@ export const projects: projectI[] = [
       'devicon-postgresql-plain',
       'devicon-docker-plain',
     ],
-    url: 'https://www.youtube.com/watch?v=kyZ7U-El2OY',
   },
 ];
 
@@ -103,5 +97,4 @@ export interface projectI {
   color: string,
   images: string[],
   technologies: string[],
-  url: string
 }
