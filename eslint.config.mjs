16801f46a0f1ea2f49c712e6edcf3f180726a882
Pedro Holguin <ogti016@dirislimanorte.gob.pe>
2025-07-17 22:47:04 -0500

    feat -New portfolio

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
     files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     rules: {
       "prefer-const": "off", 
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
